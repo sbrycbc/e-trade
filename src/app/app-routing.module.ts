2585b4c5bfa4c65d2d919150af4bb0413be07630
sbrycbc <sabriyecbc@gmail.com>
2024-10-29 11:56:54 +0100

    added home component..updated navbar with styling...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutsComponent } from './components/layouts/layouts.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 
 const routes: Routes = [
   {
     path:"",
-    component: LayoutsComponent
+    component: LayoutsComponent,
+    children:[
+      {
+        path:"",
+        component: HomeComponent
+      }
+    ]
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LayoutsComponent } from './components/layouts/layouts.component';  
+import { HomeComponent } from './components/home/home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutsComponent 
+    LayoutsComponent, 
+    HomeComponent
   ],
   imports: [
     BrowserModule,
